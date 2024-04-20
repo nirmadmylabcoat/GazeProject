8278538bde288ac266ba3cb3be98d173c7a9aabd
Sanjana R <139480944+nirmadmylabcoat@users.noreply.github.com>
2024-04-20 19:28:31 +0530

    prod display for lg done

--- src/pages/ProductDisplay.jsx
+++ src/pages/ProductDisplay.jsx
@@ -4,6 +4,9 @@ import Header from "../components/Global/Header.jsx";
 import itemData from "../components/ProductList/Data.jsx";
 import CustomCursor from "../components/Global/CustomCursor";
 import Transition from "../Transition";
+import { FaStoreAlt } from "react-icons/fa";
+import { TbTruckDelivery } from "react-icons/tb";
+
 
 const ProductDisplay = () => {
   const { id } = useParams();
@@ -16,7 +19,7 @@ const ProductDisplay = () => {
       <CustomCursor />
       <Header delay={"0"} duration={"0"} />
       <div className="w-[100vw] flex justify-center">
-        <div className="w-[90vw] flex gap-x-20 mt-10">
+        <div className="w-[90vw] flex gap-x-20 mt-10 mb-10">
           <div
             className="h-[75vh] flex gap-x-2"
             // onMouseEnter={() => setHovered(index)} // Set this item as hovered
@@ -26,16 +29,16 @@ const ProductDisplay = () => {
               // src={hovered === index ? item.imageHover : item.image}
               src={item.image}
               alt={item.name}
-              className="h-full object-cover"
+              className="h-full w-[400px] object-cover"
             />
             <img
               // src={hovered === index ? item.imageHover : item.image}
               src={item.imageHover}
               alt={item.name}
-              className="h-full object-cover"
+              className="h-full w-[400px] object-cover"
             />
           </div>
-          <div>
+          <div className="flex flex-col gap-y-8 max-w-[30vw]">
             <div className="flex flex-col gap-y-[0.25rem]">
               <div className="font-semibold text-[3rem] text-gray-800">
                 {item.name}
@@ -44,23 +47,37 @@ const ProductDisplay = () => {
                 <p className="text-gray-500 font-light">
                   MRP inclusive of all taxes
                 </p>
-                <p className="font-medium text-gray-800 text-3xl">Rs. {item.price}</p>
+                <p className="font-medium text-gray-800 text-3xl">
+                  Rs. {item.price}
+                </p>
               </div>
             </div>
-            <div>
+            <div className="font-medium text-gay-800 text-2xl flex ">
+              {item.description}
+            </div>
+            <div className="flex flex-col gap-y-2">
               <div>
-                <p>Sizes</p>
+                <p className="text-xl">Sizes</p>
               </div>
-              <div>
-                <div>XS</div>
-                <div>S</div>
-                <div>M</div>
-                <div>L</div>
-                <div>XL</div>
+              <div className="flex gap-x-[1rem]">
+                <div className="text-2xl border-solid border-gray-400 hover:border-gray-600 transition-all duration-300 border-[1px] px-[32px] py-[10px]">XS</div>
+                <div className="text-2xl border-solid border-gray-400 hover:border-gray-600 transition-all duration-300 border-[1px] px-[32px] py-[10px]">S</div>
+                <div className="text-2xl border-solid border-gray-400 hover:border-gray-600 transition-all duration-300 border-[1px] px-[32px] py-[10px]">M</div>
+                <div className="text-2xl border-solid border-gray-400 hover:border-gray-600 transition-all duration-300 border-[1px] px-[32px] py-[10px]">L</div>
+                <div className="text-2xl border-solid border-gray-400 hover:border-gray-600 transition-all duration-300 border-[1px] px-[32px] py-[10px]">XL</div>
               </div>
             </div>
-            <div></div>
-            <div></div>
+            <div className="flex justify-center w-full border-[4px] bg-black text-white text-[1.5rem] px-20 py-8 hover:bg-gray-700 transition-all">Add To Cart</div>
+            <div className="flex flex-col gap-y-2">
+              <div className="text-xl flex items-center gap-x-4">
+                <div><FaStoreAlt /></div>
+                <div>Available in stores</div>
+              </div>
+              <div className="text-xl flex items-center gap-x-4">
+                <div className="text-2xl"><TbTruckDelivery /></div>
+                <div>Delivery Time : 2-7 days</div>
+                </div>
+            </div>
           </div>
         </div>
       </div>
