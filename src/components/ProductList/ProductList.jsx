17c62da2217b9fb26a1ec54a6d3f6f7510529e9c
Sanjana R <139480944+nirmadmylabcoat@users.noreply.github.com>
2024-04-20 18:07:02 +0530

    product display update

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,38 +1,53 @@
-import React, { useState } from 'react';
-import itemData from './Data';
+import React, { useState } from "react";
+import itemData from "./Data";
+import { Link } from "react-router-dom";
 
-function ProductList({gender}) {
+function ProductList({ gender }) {
   // State to keep track of which item is hovered
   const [hovered, setHovered] = useState(null);
-  const filteredProducts = itemData.filter(item => item.gender === gender)
+  const filteredProducts = itemData.filter((item) => item.gender === gender);
 
   return (
     <div className="mt-20 mb-20 w-[100vw] flex justify-center">
       <div className="w-[90vw] flex flex-wrap gap-y-[5rem] justify-center">
         {filteredProducts.map((item, index) => {
           return (
-            <div 
+            <div
               key={index} // It's good to have a key for list items
-              className="flex flex-col w-[30rem] min-w-[180px] p-1"
-              onMouseEnter={() => setHovered(index)} // Set this item as hovered
-              onMouseLeave={() => setHovered(null)} // Reset when not hovered
+              className="flex flex-col w-[30rem] min-w-[180px] p-1 hover:shadow-md transition-all duration-300"
             >
-              <div className="overflow-hidden h-[100%]">
-                <img 
+              <div
+                className="overflow-hidden h-[100%]"
+                onMouseEnter={() => setHovered(index)} // Set this item as hovered
+                onMouseLeave={() => setHovered(null)} // Reset when not hovered
+              >
+                <img
                   src={hovered === index ? item.imageHover : item.image}
                   alt={item.name}
-                  className="h-full object-cover" 
+                  className="h-full object-cover"
                 />
               </div>
-              <div className="mt-[2rem]">
-                <p className="text-gray-400 uppercase text-[1.25rem] font-[650]">{item.type}</p>
-              </div>
-              <div>
-                <p className="text-[1.75rem] font-bold text-gray-700">{item.name}</p>
-              </div>
-              <div className="mb-[2rem]">
-                <p className="font-semibold text-gray-700">Rs. {item.price}</p>
-              </div>
+              <Link to={`/product/${item.id}`}>
+                <div className="pl-5">
+                  <div className="mt-[2rem]">
+                    <p className="text-gray-400 uppercase text-[1.25rem] font-[650]">
+                      {item.type}
+                    </p>
+                  </div>
+
+                  <div>
+                    <p className="text-[1.75rem] font-bold text-gray-700">
+                      {item.name}
+                    </p>
+                  </div>
+
+                  <div className="mb-[2rem]">
+                    <p className="font-semibold text-gray-700">
+                      Rs. {item.price}
+                    </p>
+                  </div>
+                </div>
+              </Link>
             </div>
           );
         })}
--- src/pages/ProductDisplay.jsx
+++ src/pages/ProductDisplay.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import Header from "../components/Global/Header.jsx";
+import itemData from "../components/ProductList/Data.jsx";
+import CustomCursor from "../components/Global/CustomCursor";
+import Transition from "../Transition";
 
 const ProductDisplay = () => {
+  const { id } = useParams();
+  // const [hovered, setHovered] = useState(null);
+  const item = itemData.find((item) => {
+    return item.id === parseInt(id);
+  });
   return (
-    <div>ProductDisplay</div>
-  )
-}
+    <>
+      <CustomCursor />
+      <Header delay={"0"} duration={"0"} />
+      <div className="w-[100vw] flex justify-center">
+        <div className="w-[90vw] flex gap-x-20 mt-10">
+          <div
+            className="h-[75vh] flex gap-x-2"
+            // onMouseEnter={() => setHovered(index)} // Set this item as hovered
+            // onMouseLeave={() => setHovered(null)} // Reset when not hovered
+          >
+            <img
+              // src={hovered === index ? item.imageHover : item.image}
+              src={item.image}
+              alt={item.name}
+              className="h-full object-cover"
+            />
+            <img
+              // src={hovered === index ? item.imageHover : item.image}
+              src={item.imageHover}
+              alt={item.name}
+              className="h-full object-cover"
+            />
+          </div>
+          <div>
+            <div className="flex flex-col gap-y-[0.25rem]">
+              <div className="font-semibold text-[3rem] text-gray-800">
+                {item.name}
+              </div>
+              <div>
+                <p className="text-gray-500 font-light">
+                  MRP inclusive of all taxes
+                </p>
+                <p className="font-medium text-gray-800 text-3xl">Rs. {item.price}</p>
+              </div>
+            </div>
+            <div>
+              <div>
+                <p>Sizes</p>
+              </div>
+              <div>
+                <div>XS</div>
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+              </div>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ProductDisplay
+export default Transition(ProductDisplay);
